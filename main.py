e53e69233834c91d2ad1d36c9ec7aba7650c942e
Ricardo Diaz <ing.rdiaz@gmail.com>
2021-11-25 15:03:35 -0500

    tweet_post commit

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ class Tweet(BaseModel):
         max_length=256
     )
     created_at: datetime = Field(default=datetime.now())
-    update_at: Optional[datetime] = Field(default=None)
+    updated_at: Optional[datetime] = Field(default=None)
     by: User = Field(...)
 
 # Path Operations
@@ -93,13 +93,14 @@ def signup(
     - last_name: str
     - birth_date: date
     """
-    #open file user.jason in read/write mode with utf-8 encoding
+    
+    #open file user.json in read/write mode with utf-8 encoding
     with open("users.json", "r+", encoding="utf-8") as f:
-        # read file and take the string and transfor as json and loda in result
+        # read file and take the string and transform as json and loda in result
         results = json.loads(f.read())
         # transform the request body in dictionary and save in variable dict
         user_dict = user.dict()
-        # user_id and birth_date as not string and use cast for transform in dict
+        # user_id and birth_date are not string and use cast for transform in dict
         user_dict["user_id"] = str(user_dict["user_id"])
         user_dict["birth_date"] = str(user_dict["birth_date"])
         # add user dictionary to results variable
@@ -206,8 +207,48 @@ def home():
     summary="Post a tweet",
     tags=["Tweet"]
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    
+    """
+    ## Post a Tweet
+
+    This path operation post a tweet in the app
+
+    **Parameters**
+
+    - Request body parameter
+        - tweet: Tweet
+
+    Return a json with the basic tweet information:
+
+    - tweet_id: UUID
+    - content: str
+    - created_at: datetime
+    - updated_at: datetime
+    - by: User
+    """
+    #open file tweets.json in read/write mode with utf-8 encoding
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        # read file and take the string and transform as json and loda in result
+        results = json.loads(f.read())
+        # transform the request body in dictionary and save in variable dict
+        tweet_dict = tweet.dict()
+        # tweet_id, created_at and update_at are not string and use cast for transform in dict
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        # user_id and birth_date are not string and use cast for transform in dict
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+
+        # add user dictionary to results variable
+        results.append(tweet_dict)
+
+        # move the firts line in file
+        f.seek(0)
+        # write in the file and transform a list of dict -results- an a json
+        f.write(json.dumps(results))
+        return tweet
 
 ### Show a tweet
 @app.get(
